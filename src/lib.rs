bbf327926fe2adb6cac6365f1937c37fae306ab8
codetiger <nharishankar@gmail.com>
2024-09-22 12:11:50 +0530

    Improved main to convert all sample xml files in the folder
    Added support for converting all xml files in the xml folder into JSON for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
 // Auto-generated lib.rs
 pub mod message;
+
+use serde::{Deserialize, Serialize};
+use std::cmp::PartialEq;
+
+use crate::message::fednow::fednow_incoming_external::FedNowIncomingMessage;
+use crate::message::fednow::fednow_outgoing_external::FedNowOutgoingMessage;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct FednowMessage {
+    #[serde(rename = "FedNowIncomingMessage")]
+    pub fed_now_incoming_message: Box<Option<FedNowIncomingMessage>>,
+
+    #[serde(rename = "FedNowOutgoingMessage")]
+    pub fed_now_outgoing_message: Box<Option<FedNowOutgoingMessage>>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use serde_xml_rs::from_reader;
 use serde_path_to_error::deserialize;
-use std::fs::File;
-use std::io::BufReader;
+use std::fs::{self, File};
+use std::io::{BufReader, Write};
+use std::path::Path;
 use xml::reader::EventReader;
 use serde_json::json;
 
-use messages::message::fednow::fednow_incoming_external::FedNowIncoming;
+use messages::FednowMessage;
 
 fn main() {
-    let file = File::open("xml/fednow-sample-message-pacs008.xml").expect("Unable to open file");
+    // Define the directory containing XML files
+    let xml_directory = "xml";
+
+    // Iterate over all files in the directory
+    for entry in fs::read_dir(xml_directory).expect("Unable to read directory") {
+        let entry = entry.expect("Unable to get directory entry");
+        let path = entry.path();
+
+        // Check if the file has an .xml extension
+        if path.extension().and_then(|e| e.to_str()) == Some("xml") {
+            // Parse the XML file and convert to JSON
+            parse_and_convert_to_json(&path);
+        }
+    }
+}
+
+fn parse_and_convert_to_json(xml_path: &Path) {
+    // Open the XML file
+    let file = File::open(xml_path).expect("Unable to open file");
     let reader = BufReader::new(file);
 
     // Wrap the BufReader in an EventReader
@@ -16,14 +35,24 @@ fn main() {
 
     // Use serde_path_to_error to get the path where deserialization failed
     let mut deserializer = serde_xml_rs::Deserializer::new(event_reader);
-    let result: Result<FedNowIncoming, serde_path_to_error::Error<serde_xml_rs::Error>> = deserialize(&mut deserializer);
+    let result: Result<FednowMessage, serde_path_to_error::Error<serde_xml_rs::Error>> = deserialize(&mut deserializer);
 
     match result {
         Ok(data) => {
-            println!("Successfully deserialized: {}", serde_json::to_string_pretty(&data).unwrap());
+            // Convert to pretty JSON
+            let json_data = serde_json::to_string_pretty(&data).unwrap();
+
+            // Create the new JSON file path (same name as XML but with .json extension)
+            let json_file_path = xml_path.with_extension("json");
+
+            // Write the JSON data to the new file
+            let mut json_file = File::create(&json_file_path).expect("Unable to create JSON file");
+            json_file.write_all(json_data.as_bytes()).expect("Unable to write JSON data");
+
+            println!("Successfully converted {} to JSON.", xml_path.display());
         },
         Err(e) => {
-            println!("Deserialization error at path: {}", e.path());
+            println!("Deserialization error in {} at path: {}", xml_path.display(), e.path());
             println!("Full error: {}", e);
         }
     }
